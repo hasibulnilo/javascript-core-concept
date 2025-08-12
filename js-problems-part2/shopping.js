32d3a470fcda3f6b2f240942e6b746acb5b2b9fe
hasibulnilo <hasibulislamniloy142@gmail.com>
2025-08-12 21:07:23 +0600

    added problem1 task

---
+++ js-problems-part2/shopping.js
@@ -0,0 +1,20 @@
+const products = [
+  { name: "shampoo", price: 300, quantity: 2 },
+  { name: "Chiruni", price: 100, quantity: 3 },
+  { name: "shirt", price: 700, quantity: 5 },
+  { name: "pant", price: 1200, quantity: 1 },
+];
+
+function cartTotal(products) {
+  
+  let total = 0;
+  
+    for (const product of products) {
+        const thisProductcost = product.price * product.quantity;
+    total = total + thisProductcost; 
+  }
+  return total;
+}
+
+const shoppingCost = cartTotal(products);
+console.log(shoppingCost);
