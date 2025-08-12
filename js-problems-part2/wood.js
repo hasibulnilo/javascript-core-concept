2b7f68da362f49132b12c3c903b48b791d6369a5
hasibulnilo <hasibulislamniloy142@gmail.com>
2025-08-12 17:43:05 +0600

    added problem1 task

---
+++ js-problems-part2/wood.js
@@ -0,0 +1,24 @@
+/*
+ *chair --->3cft
+ *table --->1cft
+ *bed --->50cft
+ */
+
+function woodQuantity(chairQuantity, tableQuantity, bedQuantity) {
+  const perchairWood = 3;
+  const perTableWood = 10;
+  const perBedWood = 50;
+
+  const allChairWood = chairQuantity * perchairWood;
+  const tableTotalWood = tableQuantity * perTableWood;
+  const bedTotalwood = bedQuantity * perBedWood;
+
+
+
+  const totalWood = allChairWood + tableTotalWood + bedTotalwood; 
+
+  return totalWood;
+} 
+
+const wood = woodQuantity(0,0,1);
+console.log('wood needed', wood);
