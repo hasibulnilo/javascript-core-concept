087e1bdd36a7cc101c2ceeadc279b4f761ad564b
hasibulnilo <hasibulislamniloy142@gmail.com>
2025-08-29 16:19:14 +0600

    upadation  added

---
+++ Tour of Dom/js/cssInJS.js
@@ -0,0 +1,21 @@
+// const sections = document.getElementsByTagName('section');
+const sections = document.querySelectorAll("section");
+
+//   console.log(sections);
+//   for (const section of sections) {
+//   console.log(section);
+//   section.style.backgroundColor = 'lightblue';
+//   section.style.border = '3px solid green';
+//   section.style.marginBlock = '5px';
+//   section.style.borderRadius = "15px";
+//   section.style,padding = "10px";
+//  }
+
+
+
+
+
+// Dynamic class
+// for (const section of sections) {
+// section.classList.add("section-card");
+// }
---
+++ Tour of Dom/js/first.js
@@ -0,0 +1,12 @@
+console.log("fisrst file");
+
+const allSections = document.getElementsByTagName("section");
+
+console.log(allSections);
+
+
+
+const firstTitle = document.getElementById('first-tittle');
+console.log(firstTitle.innerText);
+
+firstTitle.innerText =" Niloy" 
